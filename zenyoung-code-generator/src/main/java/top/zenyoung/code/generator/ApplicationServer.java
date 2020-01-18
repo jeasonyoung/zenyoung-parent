3ac61416cef5bf7ac9e98e1fe0c198553e50f83e
杨勇 <yangyong@wosaiapp.com>
2020-01-18 17:53:52 +0800

    添加代码生成器模块

--- zenyoung-code-generator/src/main/java/top/zenyoung/code/generator/ApplicationServer.java
+++ zenyoung-code-generator/src/main/java/top/zenyoung/code/generator/ApplicationServer.java
@@ -1,18 +1,23 @@
 package top.zenyoung.code.generator;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
- * App 入口
+ * 代码生成器入口
+ *
  * @author yangyong
  * @version 1.0
  * @date 2020/1/18 4:39 下午
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApplicationServer {
 
-    public static void main(final String[] args){
-        SpringApplication.run(ApplicationServer.class, args);
+    public static void main(final String[] args) {
+        new SpringApplicationBuilder(ApplicationServer.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 }
