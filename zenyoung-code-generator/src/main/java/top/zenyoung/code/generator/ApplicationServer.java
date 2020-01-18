f2d38ca24edd10592e4de468b4263cec15446fc3
杨勇 <yangyong@wosaiapp.com>
2020-01-18 16:42:36 +0800

    添加代码生成器

---
+++ zenyoung-code-generator/src/main/java/top/zenyoung/code/generator/ApplicationServer.java
@@ -0,0 +1,18 @@
+package top.zenyoung.code.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App 入口
+ * @author yangyong
+ * @version 1.0
+ * @date 2020/1/18 4:39 下午
+ **/
+@SpringBootApplication
+public class ApplicationServer {
+
+    public static void main(final String[] args){
+        SpringApplication.run(ApplicationServer.class, args);
+    }
+}
