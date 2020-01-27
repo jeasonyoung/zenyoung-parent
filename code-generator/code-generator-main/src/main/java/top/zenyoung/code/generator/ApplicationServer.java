539751669ff2056dde38eebe4b75ff0d6558475e
杨勇 <yangyong@wosaiapp.com>
2020-01-27 21:11:42 +0800

    添加JavaFx

--- code-generator/code-generator-main/src/main/java/top/zenyoung/code/generator/ApplicationServer.java
+++ code-generator/code-generator-main/src/main/java/top/zenyoung/code/generator/ApplicationServer.java
@@ -1,10 +1,17 @@
 package top.zenyoung.code.generator;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import top.zenyoung.code.generator.view.MainStageView;
-import top.zenyoung.jfx.support.AbstractJavaFxApplicationSupport;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * 代码生成器入口
@@ -15,9 +22,32 @@ import top.zenyoung.jfx.support.AbstractJavaFxApplicationSupport;
  **/
 @Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class ApplicationServer extends AbstractJavaFxApplicationSupport {
+public class ApplicationServer extends Application implements CommandLineRunner {
 
     public static void main(final String[] args) {
-        launch(ApplicationServer.class, MainStageView.class, args);
+        new SpringApplicationBuilder(ApplicationServer.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        log.debug("启动JavaFx...");
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final Text text = new Text(10, 40, "Hello World!");
+        text.setFont(new Font(40));
+        final Scene scene = new Scene(new Group(text));
+
+        stage.setTitle("代码生成器");
+        stage.setWidth(600);
+        stage.setHeight(400);
+
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
     }
 }
