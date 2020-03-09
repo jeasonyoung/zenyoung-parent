2ab08902b799a268763990c54432c83bc4bfb6d9
杨勇 <yangyong@wosaiapp.com>
2020-03-09 13:03:56 +0800

    添加Web代码生成器代码

---
+++ code-generator/code-generator-common/src/main/java/top/zenyoung/code/generator/common/CodeType.java
@@ -0,0 +1,16 @@
+package top.zenyoung.code.generator.common;
+
+import lombok.Getter;
+
+/**
+ * 代码生成类型
+ *
+ * @author yangyong
+ * @version 1.0
+ * @date 2020/3/9 12:57 下午
+ **/
+@Getter
+public enum CodeType {
+    ;
+
+}
