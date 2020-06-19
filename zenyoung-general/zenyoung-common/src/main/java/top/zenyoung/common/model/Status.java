575ee9bab8a2f7ab739abae412bf1d9505697df8
杨勇 <jeason1914@qq.com>
2020-06-19 14:36:15 +0800

    1.优化status转化方法 2.升级版本到1.2.1

--- zenyoung-general/zenyoung-common/src/main/java/top/zenyoung/common/model/Status.java
+++ zenyoung-general/zenyoung-common/src/main/java/top/zenyoung/common/model/Status.java
@@ -3,6 +3,7 @@ package top.zenyoung.common.model;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.annotation.Nullable;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,11 +34,23 @@ public enum Status implements EnumValue {
     private final int val;
     private final String title;
 
+    /**
+     * 构造函数
+     *
+     * @param val   状态值
+     * @param title 状态标题
+     */
     Status(final int val, final String title) {
         this.val = val;
         this.title = title;
     }
 
+    /**
+     * 状态转换
+     *
+     * @param val 状态值
+     * @return 状态对象
+     */
     public static Status parse(final Integer val) {
         if (val != null) {
             for (Status s : Status.values()) {
@@ -49,10 +62,28 @@ public enum Status implements EnumValue {
         return null;
     }
 
+    /**
+     * 获取非零状态集合
+     *
+     * @return 状态集合
+     */
     public static List<Status> toNormalAll() {
         return Stream.of(Status.values())
                 .filter(s -> s.getVal() > -1)
                 .sorted(Comparator.comparingInt(Status::getVal))
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 创建响应对象
+     *
+     * @param status 状态
+     * @return 响应对象
+     */
+    public static EnumData createResp(@Nullable final Status status) {
+        if (status != null) {
+            return EnumData.parse(status);
+        }
+        return null;
     }
 }
