042f023d942bfd9e9c2be1453716cc35d839e12c
杨勇 <jeason1914@qq.com>
2020-06-23 15:04:04 +0800

    1.添加WebClient接口 2.升级版本到1.3.1

--- zenyoung-general/zenyoung-webclient-okhttp3/src/main/java/top/zenyoung/webclient/WebClientUtils.java
+++ zenyoung-general/zenyoung-webclient-okhttp3/src/main/java/top/zenyoung/webclient/WebClientUtils.java
@@ -1,6 +1,5 @@
 package top.zenyoung.webclient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
@@ -23,7 +22,7 @@ import java.util.function.Supplier;
  * date 2020/6/23 10:33 上午
  **/
 @Slf4j
-public class WebClientUtils {
+public class WebClientUtils implements WebClient {
     private static final int TIMEOUT = 10;
     private final OkHttpClient client;
 
@@ -61,6 +60,7 @@ public class WebClientUtils {
      * @return 返回数据
      * @throws IOException 异常
      */
+    @Override
     public <R> R sendRequest(@Nonnull final String method, @Nonnull final String url,
                              @Nullable final Map<String, Serializable> headers,
                              @Nonnull final Supplier<RequestBody> bodyConvert, @Nonnull final Function<String, R> respBodyConvert) throws IOException {
@@ -83,120 +83,6 @@ public class WebClientUtils {
         final ResponseBody respBody = response.body();
         //结果转换
         return respBodyConvert.apply(respBody == null ? null : respBody.string());
-    }
-
-    /**
-     * 发送JSON请求
-     *
-     * @param method    请求方法
-     * @param url       请求地址
-     * @param headers   请求头参数
-     * @param body      请求报文体
-     * @param objMapper JSON处理器
-     * @param respClass 响应类型Class
-     * @param <T>       请求报文体类型
-     * @param <R>       响应数据类型
-     * @return 响应数据
-     * @throws IOException 异常
-     */
-    public <T, R> R sendJson(@Nonnull final String method, @Nonnull final String url,
-                             @Nullable final Map<String, Serializable> headers, @Nullable final T body,
-                             @Nonnull final ObjectMapper objMapper, @Nonnull final Class<R> respClass) throws IOException {
-        log.debug("sendJson(method: {},url: {},headers: {},body: {},objMapper: {},respClass: {})...", method, url, headers, body, objMapper, respClass);
-        return sendRequest(method, url, headers, () -> {
-            if (body != null) {
-                try {
-                    final String bodyJson = objMapper.writeValueAsString(body);
-                    return RequestBody.create(MediaType.parse("application/json;charset=utf-8"), bodyJson);
-                } catch (Throwable ex) {
-                    log.error("sendJson-writeValueAsString(body: {})-exp: {}", body, ex.getMessage());
-                    throw new RuntimeException(ex);
-                }
-            }
-            return null;
-        }, resp -> {
-            if (!Strings.isNullOrEmpty(resp)) {
-                try {
-                    return objMapper.readValue(resp, respClass);
-                } catch (Throwable ex) {
-                    log.error("sendPostJson-readValue(resp: {})-exp: {}", resp, ex.getMessage());
-                    throw new RuntimeException(ex);
-                }
-            }
-            return null;
-        });
-    }
-
-    /**
-     * 发送GET-JSON请求
-     *
-     * @param url       请求地址
-     * @param headers   请求头参数
-     * @param objMapper JSON处理器
-     * @param respClass 响应类型Class
-     * @param <R>       响应数据类型
-     * @return 响应数据
-     * @throws IOException 异常
-     */
-    public <R> R sendGetJson(@Nonnull final String url, @Nullable final Map<String, Serializable> headers,
-                             @Nonnull final ObjectMapper objMapper, @Nonnull final Class<R> respClass) throws IOException {
-        log.debug("sendGetJson(url: {},headers: {},objMapper: {},respClass: {})...", url, headers, objMapper, respClass);
-        return sendJson("GET", url, headers, null, objMapper, respClass);
-    }
-
-    /**
-     * 发送POST-JSON请求
-     *
-     * @param url       请求地址
-     * @param headers   请求头参数
-     * @param body      请求报文体
-     * @param objMapper JSON处理器
-     * @param respClass 响应类型Class
-     * @param <T>       请求报文体类型
-     * @param <R>       响应数据类型
-     * @return 响应数据
-     * @throws IOException 异常
-     */
-    public <T, R> R sendPostJson(@Nonnull final String url, @Nullable final Map<String, Serializable> headers, @Nullable final T body,
-                                 @Nonnull final ObjectMapper objMapper, @Nonnull final Class<R> respClass) throws IOException {
-        log.debug("sendPostJson(url: {},headers: {},body: {},objMapper: {},respClass: {})...", url, headers, body, objMapper, respClass);
-        return sendJson("POST", url, headers, body, objMapper, respClass);
-    }
-
-    /**
-     * 发送PUT-JSON请求
-     *
-     * @param url       请求地址
-     * @param headers   请求头参数
-     * @param body      请求报文体
-     * @param objMapper JSON处理器
-     * @param respClass 响应类型Class
-     * @param <T>       请求报文体类型
-     * @param <R>       响应数据类型
-     * @return 响应数据
-     * @throws IOException 异常
-     */
-    public <T, R> R sendPutJson(@Nonnull final String url, @Nullable final Map<String, Serializable> headers, @Nullable final T body,
-                                @Nonnull final ObjectMapper objMapper, @Nonnull final Class<R> respClass) throws IOException {
-        log.debug("sendPutJson(url: {},headers: {},body: {},objMapper: {},respClass: {})...", url, headers, body, objMapper, respClass);
-        return sendJson("PUT", url, headers, body, objMapper, respClass);
-    }
-
-    /**
-     * 发送DELETE-JSON请求
-     *
-     * @param url       请求地址
-     * @param headers   请求头参数
-     * @param objMapper JSON处理器
-     * @param respClass 响应类型Class
-     * @param <R>       响应数据类型
-     * @return 响应数据
-     * @throws IOException 异常
-     */
-    public <R> R sendDeleteJson(@Nonnull final String url, @Nullable final Map<String, Serializable> headers,
-                                @Nonnull final ObjectMapper objMapper, @Nonnull final Class<R> respClass) throws IOException {
-        log.debug("sendDeleteJson(url: {},headers: {},objMapper: {},respClass: {})...", url, headers, objMapper, respClass);
-        return sendJson("DELETE", url, headers, null, objMapper, respClass);
     }
 
     private static class HttpLogging implements HttpLoggingInterceptor.Logger {
