6fb3deb93306f35c44c83dee2f88a561848078e1
杨勇 <jeason1914@qq.com>
2020-07-07 15:37:39 +0800

    1.优化定时任务服务基础类 2.升级版本到1.5.3

--- zenyoung-general/zenyoung-service/src/main/java/top/zenyoung/service/impl/AbstractTaskServiceImpl.java
+++ zenyoung-general/zenyoung-service/src/main/java/top/zenyoung/service/impl/AbstractTaskServiceImpl.java
@@ -1,12 +1,15 @@
 package top.zenyoung.service.impl;
 
+import com.google.common.cache.Cache;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.Assert;
+import top.zenyoung.common.util.CacheUtils;
 import top.zenyoung.service.TaskService;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -17,10 +20,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  **/
 @Slf4j
 public abstract class AbstractTaskServiceImpl implements TaskService {
+    private static final Cache<String, Integer> MAX_TASK_CACHE = CacheUtils.createCache(100, 30, TimeUnit.SECONDS);
     private static final Map<String, Object> LOCKS = Maps.newConcurrentMap();
-    private static final Map<String, AtomicInteger> TASK_CONCURRENCY_TOTALS = Maps.newConcurrentMap();
+    private static final Map<String, AtomicInteger> TASK_COUTERS = Maps.newConcurrentMap();
 
-    private static final int MAX = Runtime.getRuntime().availableProcessors() * 10;
+    /**
+     * 默认支持的最大并发数(CPU内核数)
+     */
+    protected static final int DEF_MAX = Runtime.getRuntime().availableProcessors();
 
     /**
      * 获取任务最大并发数
@@ -28,37 +35,41 @@ public abstract class AbstractTaskServiceImpl implements TaskService {
      * @param key 任务键
      * @return 最大并发数
      */
-    protected int getTaskMaxConcurrent(@Nonnull final String key) {
-        log.debug("getTaskMaxConcurrent(key: {})...", key);
-        return MAX;
+    @Nonnull
+    protected abstract Integer getMaxTaskConcurrents(@Nonnull final String key);
+
+    private int getMaxConcurrents(@Nonnull final String key) {
+        final String lock = key + "-max_task";
+        synchronized (LOCKS.computeIfAbsent(lock, k -> new Object())) {
+            try {
+                final Integer val = CacheUtils.getCacheValue(MAX_TASK_CACHE, key, () -> getMaxTaskConcurrents(key));
+                return (val == null || val <= 0) ? DEF_MAX : val;
+            } finally {
+                LOCKS.remove(lock);
+            }
+        }
     }
 
     @Override
     public void startTask(@Nonnull final String key, @Nonnull final Runnable process) {
         log.debug("startTask(key: {},process: {})...", key, process);
         Assert.hasText(key, "'key'不能为空!");
-        synchronized (LOCKS.computeIfAbsent(key, k -> new Object())) {
-            try {
-                //并行阀值
-                final int max = getTaskMaxConcurrent(key);
-                //检查并行是否超过阀值
-                final AtomicInteger refCounter = TASK_CONCURRENCY_TOTALS.computeIfAbsent(key, k -> new AtomicInteger(0));
-                if (refCounter.get() > MAX) {
-                    log.warn("startTask(key: {},process: {})-并行超过阀值[{}/{}],请求稍后", key, process, refCounter, max);
-                    return;
-                }
-                //计数器累加
-                refCounter.incrementAndGet();
-                //执行任务
-                try {
-                    process.run();
-                } finally {
-                    //计数器递减
-                    refCounter.decrementAndGet();
-                }
-            } finally {
-                LOCKS.remove(key);
-            }
+        //最大并发数
+        final int max = getMaxConcurrents(key);
+        //获取当前计数器
+        final AtomicInteger refCounter = TASK_COUTERS.computeIfAbsent(key, k -> new AtomicInteger(0));
+        if (refCounter.get() >= max) {
+            log.warn("startTask(key: {},process: {})-并行超过阀值[{}/{}],请求稍后", key, process, refCounter, max);
+            return;
+        }
+        //计数器累加
+        refCounter.incrementAndGet();
+        try {
+            //执行业务
+            process.run();
+        } finally {
+            //计数器递减
+            refCounter.decrementAndGet();
         }
     }
 }
