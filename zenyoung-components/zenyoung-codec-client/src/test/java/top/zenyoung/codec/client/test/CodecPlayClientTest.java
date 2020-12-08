d875d9432fa640ebbae7c1704d43c1c8b732bfad
YANGYONG-X1C\young <jeason1914@qq.com>
2020-12-08 22:51:12 +0800

    增加签名测试处理

--- zenyoung-components/zenyoung-codec-client/src/test/java/top/zenyoung/codec/client/test/CodecPlayClientTest.java
+++ zenyoung-components/zenyoung-codec-client/src/test/java/top/zenyoung/codec/client/test/CodecPlayClientTest.java
@@ -6,25 +6,36 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-import top.zenyoung.codec.client.CodecUploadClient;
-import top.zenyoung.codec.client.CodecUploadClientDefault;
-import top.zenyoung.codec.client.vo.UploadAuthorize;
+import top.zenyoung.codec.client.CodecPlayClient;
+import top.zenyoung.codec.client.CodecPlayClientDefault;
+import top.zenyoung.codec.client.vo.PlayTicket;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 public class CodecPlayClientTest {
 
-    private CodecUploadClient uploadClient;
+    private CodecPlayClient playClient;
 
     @Before
     public void beforeInit() {
-        this.uploadClient = CodecUploadClientDefault.getInstance("http://baidu.com", "acount", "passwd");
+        this.playClient = CodecPlayClientDefault.getInstance("https://cvod.chaosw.com", "webplay", "adec4dea9478");
     }
 
     @Test
-    public void testCreateAuthorize() {
-        log.info("testCreateAuthorize....");
-        final UploadAuthorize authorize = this.uploadClient.createAuthorize("bucket", "bizId", "dir", null);
-        log.info("authorize=> {}", authorize);
+    public void playUrlTest() {
+        final String url = this.playClient.getPlayUrl(new PlayTicket() {
+
+            @Override
+            public String getToken() {
+                return "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJiOWZmNzhmNS1jZTBmLTQzZDUtOWUzZC1lOWU2ZTEwNWFlMzYiLCJpZCI6ImIwYTQxY2QwYWNiMjMwMWYyMTJiZGJjMDI5ZGNlZjc2IiwiZXh0cyI6bnVsbCwiYWNjb3VudCI6IndlYnBsYXkiLCJyb2xlcyI6bnVsbCwiaWF0IjoxNjA3NDA2MTMxLCJuYmYiOjE2MDc0MDYxMzEsImV4cCI6MTYwNzQxMDYzNn0.Pp8O71n6WaArAU5vgFMH64iKpmUAlnAm4s1iyt2BkkGJzdsXq7o-a4Aq6Kl8-QYl1y363hmFRG5wAqjMXQXFZw";
+            }
+
+            @Override
+            public Long getExpire() {
+                return 1604814799000L;
+            }
+        }, "other");
+        log.info("play-url=> {}", url);
     }
+
 }
