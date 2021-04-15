efa8603b488e5eebea9acf4f6dccb216c6d5e462
YANGYONG-X1C\young <jeason1914@gmail.com>
2021-04-15 09:36:55 +0800

    优化枚举对象

--- zenyoung-common/src/main/java/top/zenyoung/common/model/EnumData.java
+++ zenyoung-common/src/main/java/top/zenyoung/common/model/EnumData.java
@@ -1,7 +1,6 @@
 package top.zenyoung.common.model;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import javax.annotation.Nullable;
 
@@ -12,7 +11,6 @@ import javax.annotation.Nullable;
  * @version 1.0
  **/
 @Getter
-@RequiredArgsConstructor(staticName = "of")
 public class EnumData implements EnumValue {
     /**
      * 枚举值
@@ -22,6 +20,17 @@ public class EnumData implements EnumValue {
      * 枚举标题
      */
     private final String title;
+
+    /**
+     * 构造函数
+     *
+     * @param val   枚举值
+     * @param title 枚举标题
+     */
+    public EnumData(final int val, final String title) {
+        this.val = val;
+        this.title = title;
+    }
 
     /**
      * 枚举数据转换
@@ -36,12 +45,23 @@ public class EnumData implements EnumValue {
     /**
      * 静态构建枚举数据
      *
+     * @param val   枚举值
+     * @param title 枚举标题
+     * @return 枚举数据
+     */
+    public static EnumData of(final int val, final String title) {
+        return new EnumData(val, title);
+    }
+
+    /**
+     * 静态构建枚举数据
+     *
      * @param data 枚举数据接口
      * @return 枚举数据
      */
     public static EnumData of(@Nullable final EnumValue data) {
         if (data != null) {
-            return new EnumData(data.getVal(), data.getTitle());
+            return EnumData.of(data.getVal(), data.getTitle());
         }
         return null;
     }
