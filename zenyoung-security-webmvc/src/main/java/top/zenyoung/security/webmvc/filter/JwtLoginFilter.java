aa3d9204ef54309f6bc4b3a9cf2d324a6d90a85a
YANGYONG-X1C\young <jeason1914@gmail.com>
2021-04-19 00:28:40 +0800

    优化代码

--- zenyoung-security-webmvc/src/main/java/top/zenyoung/security/webmvc/filter/JwtLoginFilter.java
+++ zenyoung-security-webmvc/src/main/java/top/zenyoung/security/webmvc/filter/JwtLoginFilter.java
@@ -5,23 +5,33 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.RequestPath;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.CollectionUtils;
+import top.zenyoung.common.model.UserPrincipal;
 import top.zenyoung.security.model.LoginReqBody;
+import top.zenyoung.security.model.LoginRespBody;
 import top.zenyoung.security.model.TokenAuthentication;
 import top.zenyoung.security.webmvc.ZyAuthenticationManager;
+import top.zenyoung.web.controller.util.RespJsonUtils;
+import top.zenyoung.web.vo.RespResult;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,7 +56,7 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
     private void buildRequestMatchers(@Nonnull final List<RequestMatcher> requestMatchers, @Nonnull final ZyAuthenticationManager manager) {
         final HttpMethod method = manager.getLoginMethod();
         final List<String> loginUrls = Lists.newArrayList(manager.getLoginUrls());
-        if (CollectionUtils.isEmpty(loginUrls)) {
+        if (!CollectionUtils.isEmpty(loginUrls)) {
             requestMatchers.addAll(
                     loginUrls.stream()
                             .filter(url -> !Strings.isNullOrEmpty(url))
@@ -65,7 +75,7 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
                 }
             }
         }
-        return super.requiresAuthentication(request, response);
+        return false;
     }
 
     @Override
@@ -90,5 +100,33 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
             }
         }
         return super.attemptAuthentication(servletRequest, servletResponse);
+    }
+
+    @Override
+    protected void successfulAuthentication(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain, final Authentication authResult) throws IOException, ServletException {
+        log.debug("successfulAuthentication(chain: {},authResult: {})...", chain, authResult);
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+        if (this.eventPublisher != null) {
+            this.eventPublisher.publishEvent(new InteractiveAuthenticationSuccessEvent(authResult, this.getClass()));
+        }
+        try {
+            //登录成功处理
+            final UserPrincipal principal = (UserPrincipal) authResult.getPrincipal();
+            if (principal != null) {
+                final LoginRespBody respBody = this.manager.createRespBody(principal);
+                RespJsonUtils.buildSuccessResp(objectMapper, response, respBody);
+            }
+        } catch (Throwable ex) {
+            log.error("successfulAuthentication(chain: {},authResult: {})-exp: {}", chain, authResult, ex.getMessage());
+            unsuccessfulAuthentication(request, response, new AuthenticationException(ex.getMessage(), ex) {
+            });
+        }
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException failed) throws IOException, ServletException {
+        log.debug("unsuccessfulAuthentication(failed: {})...", failed == null ? null : failed.getMessage());
+        SecurityContextHolder.clearContext();
+        RespJsonUtils.buildFailResp(objectMapper, response, HttpStatus.UNAUTHORIZED, failed);
     }
 }
