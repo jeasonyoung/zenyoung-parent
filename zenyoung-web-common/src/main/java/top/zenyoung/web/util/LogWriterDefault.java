0929fc298ffaaf15f025062128ca47a7c9474f79
YANGYONG-X1C\young <jeason1914@gmail.com>
2021-05-31 17:07:41 +0800

    优化日志输出

--- zenyoung-web-common/src/main/java/top/zenyoung/web/util/LogWriterDefault.java
+++ zenyoung-web-common/src/main/java/top/zenyoung/web/util/LogWriterDefault.java
@@ -1,16 +1,15 @@
 package top.zenyoung.web.util;
 
-import com.google.common.base.Joiner;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,10 +23,12 @@ import java.util.Map;
 public class LogWriterDefault implements LogWriter {
     private final Map<String, Serializable> logMaps;
     private final long startStamp;
+    private final ObjectMapper objectMapper;
 
     public LogWriterDefault() {
         logMaps = Maps.newLinkedHashMap();
         startStamp = System.currentTimeMillis();
+        objectMapper = new ObjectMapper();
     }
 
     @Override
@@ -46,63 +47,24 @@ public class LogWriterDefault implements LogWriter {
         }
     }
 
-    private void renderLogContent(@Nonnull final StringBuilder builder, @Nullable final String prefix, @Nonnull final Map<?, ?> map) {
+    @SneakyThrows
+    private void renderLogContent(@Nonnull final StringBuilder builder, @Nonnull final Map<?, ?> map) {
         if (!CollectionUtils.isEmpty(map)) {
-            map.forEach((k, v) -> {
-                renderPrefix(builder, prefix).append(k).append(":");
-                if (v instanceof Map) {
-                    builder.append("{").append("\n");
-                    renderLogContent(builder, (Strings.isNullOrEmpty(prefix) ? "" : prefix) + "\t", (Map<?, ?>) v);
-                    renderPrefix(builder, prefix).append("}").append("\n");
-                } else if (v instanceof List) {
-                    builder.append("[");
-                    renderLogContent(builder, prefix, (List<?>) v);
-                    builder.append("]").append("\n");
-                } else {
-                    builder.append(v).append("\n");
-                }
-            });
+            builder.append(objectMapper.writeValueAsString(map));
         }
     }
-
-    private StringBuilder renderPrefix(@Nonnull final StringBuilder builder, @Nullable final String prefix) {
-        if (!Strings.isNullOrEmpty(prefix)) {
-            builder.append(prefix);
-        }
-        return builder;
-    }
-
-    private void renderLogContent(@Nonnull final StringBuilder builder, @Nullable final String prefix, @Nonnull final List<?> list) {
-        if (!CollectionUtils.isEmpty(list)) {
-            final List<String> vals = Lists.newLinkedList();
-            int i = 0;
-            for (Object obj : list) {
-                if (obj == null) {
-                    continue;
-                }
-                if (obj instanceof Map) {
-                    if (i++ > 1) {
-                        renderPrefix(builder, prefix).append(",").append("\n");
-                    }
-                    renderLogContent(builder, prefix, (Map<?, ?>) obj);
-                    continue;
-                }
-                vals.add(String.valueOf(obj));
-            }
-            if (!CollectionUtils.isEmpty(vals)) {
-                builder.append("\n");
-                renderPrefix(builder, prefix).append(Joiner.on(",\n").skipNulls().join(vals));
-                builder.append("\n");
-            }
-        }
-    }
-
+    
     @Override
     public CharSequence outputLogs() {
         final StringBuilder builder = new StringBuilder();
         renderMark(builder, true);
-        renderLogContent(builder, null, logMaps);
-        builder.append("\n").append("耗时: ").append(System.currentTimeMillis() - startStamp).append("ms");
+        try {
+            renderLogContent(builder, logMaps);
+        } catch (Throwable ex) {
+            log.warn("outputLogs-exp: {}", ex.getMessage());
+        } finally {
+            builder.append("\n").append("耗时: ").append(System.currentTimeMillis() - startStamp).append("ms");
+        }
         renderMark(builder, false);
         return builder.toString();
     }
