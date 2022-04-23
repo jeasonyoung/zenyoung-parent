a4421069681e493288f9b2e7c6d72270e2c7d691
杨勇 <jeason1914@gmail.com>
2022-04-23 23:13:47 +0800

    添加主程序入口模块(测试)

---
+++ zenyoung-framework-main/src/main/java/top/zenyoung/framework/AppMain.java
@@ -0,0 +1,17 @@
+package top.zenyoung.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot 入口
+ *
+ * @author young
+ */
+@SpringBootApplication
+public class AppMain {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
