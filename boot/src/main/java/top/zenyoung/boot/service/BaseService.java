22fc927eab016302f4f53428ad469167fddfa7e6
杨勇 <jeason1914@gmail.com>
2022-07-17 00:52:08 +0800

    fix: 优化代码生成模块

--- boot/src/main/java/top/zenyoung/boot/service/BaseService.java
+++ boot/src/main/java/top/zenyoung/boot/service/BaseService.java
@@ -1,10 +1,12 @@
 package top.zenyoung.boot.service;
 
+import top.zenyoung.common.mapping.BeanMapping;
+
 /**
  * 服务基接口
  *
  * @author young
  */
-public interface BaseService {
+public interface BaseService extends BeanMapping {
 
 }
