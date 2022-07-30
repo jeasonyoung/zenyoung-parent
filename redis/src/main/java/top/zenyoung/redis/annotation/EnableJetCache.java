8111806fb0940312cf1331f603155ce4f13894ed
杨勇 <jeason1914@gmail.com>
2022-07-30 14:08:28 +0800

    feature: 添加redis jetcache启动集成注解

---
+++ redis/src/main/java/top/zenyoung/redis/annotation/EnableJetCache.java
@@ -0,0 +1,29 @@
+package top.zenyoung.redis.annotation;
+
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * 启用JetCache缓存
+ *
+ * @author young
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@EnableCreateCacheAnnotation
+public @interface EnableJetCache {
+    /**
+     * 扫描包集合
+     *
+     * @return 扫描包集合
+     */
+    @AliasFor(
+            annotation = EnableMethodCache.class,
+            attribute = "basePackages"
+    )
+    String[] basePackages();
+}
