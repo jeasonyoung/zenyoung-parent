c1c74fee2338403e865499ed5a770e32d557f5aa
杨勇 <jeason1914@gmail.com>
2022-09-27 11:20:40 +0800

    feature: 添加SpringMVC枚举传参支持

--- boot/src/main/java/top/zenyoung/boot/config/WebConfig.java
+++ boot/src/main/java/top/zenyoung/boot/config/WebConfig.java
@@ -1,7 +1,11 @@
 package top.zenyoung.boot.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.converter.ConverterFactory;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
@@ -9,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.zenyoung.boot.interceptor.Interceptor;
 import top.zenyoung.boot.resolver.ArgumentResolver;
+import top.zenyoung.common.model.EnumValue;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -51,6 +56,29 @@ public class WebConfig implements WebMvcConfigurer {
         //参数分解器
         if (!CollectionUtils.isEmpty(this.argumentResolvers)) {
             resolvers.addAll(this.argumentResolvers);
+        }
+    }
+
+    @Override
+    public void addFormatters(@Nonnull final FormatterRegistry registry) {
+        registry.addConverterFactory(EnumValueConvertFactory.of());
+    }
+
+    @RequiredArgsConstructor(staticName = "of")
+    private static class EnumValueConvertFactory implements ConverterFactory<Integer, EnumValue> {
+
+        @Override
+        @SuppressWarnings({"all"})
+        public <T extends EnumValue> Converter<Integer, T> getConverter(@Nonnull final Class<T> targetType) {
+            return source -> {
+                final T[] enums = targetType.getEnumConstants();
+                for (final T e : enums) {
+                    if (e.getVal() == source) {
+                        return e;
+                    }
+                }
+                return null;
+            };
         }
     }
 }
