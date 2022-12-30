fd4e448de6b71850b81048c6df6540ab590f3c3e
yangyong <jeason1914@qq.com>
2022-12-30 23:28:09 +0800

    fix:[security-web]添加Spring Web Security处理模块

---
+++ security-web/src/main/java/top/zenyoung/security/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package top.zenyoung.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import top.zenyoung.boot.util.RespJsonUtils;
+import top.zenyoung.security.filter.JwtTokenFilter;
+import top.zenyoung.security.service.AuthenManagerService;
+
+import java.util.Objects;
+
+/**
+ * 安全配置
+ *
+ * @author young
+ */
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired(required = false)
+    private AuthenManagerService manager;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        if (Objects.isNull(manager)) {
+            log.error("未配置安全认证管理器服务接口: {}", AuthenManagerService.class);
+            return;
+        }
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers(manager.getWhiteUrls()).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(manager, objectMapper), UsernamePasswordAuthenticationFilter.class)
+                .cors()
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .csrf().disable()
+                .exceptionHandling(ehc -> {
+                    //未登录处理
+                    ehc.authenticationEntryPoint((req, res, e) -> RespJsonUtils.buildFailResp(objectMapper, res, HttpStatus.UNAUTHORIZED, e));
+                    //访问拒绝处理
+                    ehc.accessDeniedHandler((req, res, e) -> RespJsonUtils.buildFailResp(objectMapper, res, HttpStatus.FORBIDDEN, e));
+                });
+    }
+}
