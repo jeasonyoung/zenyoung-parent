2f7d4241d3ebbbe03f81c217c7fab5cf83001c96
yangyong <jeason1914@qq.com>
2023-01-04 17:12:03 +0800

    fix:[common]修复枚举类型JSON化异常bug

--- common/src/main/java/top/zenyoung/common/model/Gender.java
+++ common/src/main/java/top/zenyoung/common/model/Gender.java
@@ -2,7 +2,6 @@ package top.zenyoung.common.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
