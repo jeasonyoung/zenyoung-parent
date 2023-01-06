2e522900c9e2bb6a147d429396260ff1b4bf3c7e
yangyong <jeason1914@qq.com>
2023-01-06 09:17:22 +0800

    fix:[boot]优化通用异常输出

--- boot/src/main/java/top/zenyoung/boot/controller/ErrorController.java
+++ boot/src/main/java/top/zenyoung/boot/controller/ErrorController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.zenyoung.boot.util.RespJsonUtils;
-import top.zenyoung.common.util.MapUtils;
 import top.zenyoung.common.vo.ResultVO;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * 错误异常处理-控制器
@@ -33,25 +33,31 @@ public class ErrorController extends AbstractErrorController {
         this.objMapper = objMapper;
     }
 
+    private ResultVO<?> buildResult(final HttpServletRequest req) {
+        final HttpStatus httpStatus = super.getStatus(req);
+        final Map<String, Object> body = super.getErrorAttributes(req, ErrorAttributeOptions.defaults());
+        final ResultVO<?> vo = ResultVO.ofSuccess(body);
+        if (Objects.nonNull(httpStatus)) {
+            vo.setCode(httpStatus.value());
+            vo.setMessage(httpStatus.getReasonPhrase());
+        }
+        return vo;
+    }
+
     @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
     public void errorHtml(final HttpServletRequest req, final HttpServletResponse res) {
-        final Map<String, Object> body = super.getErrorAttributes(req, ErrorAttributeOptions.defaults());
-        RespJsonUtils.buildResp(objMapper, res, getStatus(req).value(), body);
+        final HttpStatus httpStatus = super.getStatus(req);
+        final ResultVO<?> vo = buildResult(req);
+        RespJsonUtils.buildResp(objMapper, res, httpStatus.value(), vo);
     }
 
     @RequestMapping
     public ResultVO<?> error(final HttpServletRequest req) {
-        final HttpStatus status = super.getStatus(req);
-        if (status == HttpStatus.NO_CONTENT) {
-            return ResultVO.ofFail(status.getReasonPhrase());
-        }
-        final Map<String, Object> body = super.getErrorAttributes(req, ErrorAttributeOptions.defaults());
-        return MapUtils.to(body, ResultVO.class);
+        return buildResult(req);
     }
 
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     public ResultVO<?> mediaTypeNotAcceptable(final HttpServletRequest req) {
-        final Map<String, Object> body = super.getErrorAttributes(req, ErrorAttributeOptions.defaults());
-        return MapUtils.to(body, ResultVO.class);
+        return buildResult(req);
     }
 }
