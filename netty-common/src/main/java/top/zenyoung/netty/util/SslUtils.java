ea21dbbfcbbe1816d96856af864200d1052e83e4
yangyong <jeason1914@qq.com>
2023-01-06 23:30:39 +0800

    fix:[netty-common]增加SSL工具类

---
+++ netty-common/src/main/java/top/zenyoung/netty/util/SslUtils.java
@@ -0,0 +1,44 @@
+package top.zenyoung.netty.util;
+
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nonnull;
+import javax.net.ssl.SSLException;
+import java.util.Objects;
+
+/**
+ * SSL工具类
+ *
+ * @author young
+ */
+@Slf4j
+public class SslUtils {
+    private static SslContext sslContext;
+
+    static {
+        try {
+            sslContext = SslContextBuilder
+                    .forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+        } catch (SSLException e) {
+            log.error("初始化SslContext-exp: {}", e.getMessage());
+        }
+    }
+
+    public static SslContext getContext() {
+        return sslContext;
+    }
+
+    public void addSslCodec(@Nonnull final ChannelPipeline pipeline, @Nonnull final SocketChannel channel) {
+        if (Objects.nonNull(sslContext)) {
+            pipeline.addLast(sslContext.newHandler(channel.alloc()));
+        }
+    }
+
+}
