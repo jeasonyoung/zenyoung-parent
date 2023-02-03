94442b0bf2e0fcbd11788538aef6cf6592881767
yangyong <jeason1914@qq.com>
2023-02-04 02:14:48 +0800

    fix:[orm]优化BaseMapper封装方法

--- orm/src/main/java/top/zenyoung/orm/injector/BatchAddOrUpdateMethod.java
+++ orm/src/main/java/top/zenyoung/orm/injector/BatchAddOrUpdateMethod.java
@@ -1,23 +1,22 @@
 package top.zenyoung.orm.injector;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.enums.SqlMethod;
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
-import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.sql.SqlScriptUtils;
 import com.google.common.base.Joiner;
-import org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator;
-import org.apache.ibatis.executor.keygen.KeyGenerator;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import org.apache.ibatis.executor.keygen.NoKeyGenerator;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
 
 import javax.annotation.Nonnull;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -33,13 +32,13 @@ public class BatchAddOrUpdateMethod extends AbstractMethod {
 
     @Override
     public MappedStatement injectMappedStatement(final Class<?> mapperClass, final Class<?> modelClass, final TableInfo tableInfo) {
-        final List<String> cols = tableInfo.getFieldList().stream()
-                .map(TableFieldInfo::getColumn)
-                .collect(Collectors.toList());
-        cols.add(0, tableInfo.getKeyColumn());
+        final Map<String, String> colPropMaps = tableInfo.getFieldList().stream()
+                .collect(Collectors.toMap(TableFieldInfo::getColumn, TableFieldInfo::getProperty, (o, n) -> n));
+        colPropMaps.put(tableInfo.getKeyColumn(), tableInfo.getKeyProperty());
         //
+        final List<String> cols = Lists.newArrayList(colPropMaps.keySet());
         final String colScript = buildColSql(cols);
-        final String valScript = buildValSql(cols) + buildUpdateSql(cols);
+        final String valScript = buildValSql(cols, colPropMaps) + buildUpdateSql(cols);
         final String sql = String.format(SqlMethod.INSERT_ONE.getSql(), tableInfo.getTableName(), colScript, valScript);
         final SqlSource sqlSource = languageDriver.createSqlSource(configuration, sql, modelClass);
         return this.addInsertMappedStatement(mapperClass, modelClass, sqlSource, NoKeyGenerator.INSTANCE, null, null);
@@ -49,8 +48,17 @@ public class BatchAddOrUpdateMethod extends AbstractMethod {
         return LEFT_BRACKET + Joiner.on(COMMA).join(cols) + RIGHT_BRACKET;
     }
 
-    private String buildValSql(@Nonnull final List<String> cols) {
-        final String valScript = LEFT_BRACKET + cols.stream().map(field -> "#{item." + field + "}").collect(Collectors.joining(COMMA)) + RIGHT_BRACKET;
+    private String buildValSql(@Nonnull final List<String> cols, @Nonnull final Map<String, String> colPropMaps) {
+        final String valScript = LEFT_BRACKET + cols.stream()
+                .map(col -> {
+                    final String prop = colPropMaps.getOrDefault(col, null);
+                    if (!Strings.isNullOrEmpty(prop)) {
+                        return "#{item." + prop + "}";
+                    }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(COMMA)) + RIGHT_BRACKET;
         return SqlScriptUtils.convertForeach(valScript, Constants.COLL, null, "item", COMMA);
     }
 
