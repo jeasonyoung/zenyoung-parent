42d73f3362d51145a3a6401a0d7498369fdce30e
yangyong <jeason1914@qq.com>
2023-02-26 20:55:41 +0800

    fix:[boot] 增加 BaseServiceImpl protected 处理函数

--- boot/src/main/java/top/zenyoung/boot/service/impl/BaseServiceImpl.java
+++ boot/src/main/java/top/zenyoung/boot/service/impl/BaseServiceImpl.java
@@ -1,20 +1,27 @@
 package top.zenyoung.boot.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import top.zenyoung.boot.exception.ServiceException;
 import top.zenyoung.boot.service.BaseService;
 import top.zenyoung.boot.service.BeanMappingService;
 import top.zenyoung.common.mapping.BeanMapping;
+import top.zenyoung.common.model.ResultCode;
 import top.zenyoung.common.paging.PageList;
+import top.zenyoung.common.vo.ResultVO;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
 /**
  * 服务基础接口实现类
  *
  * @author young
  */
+@Slf4j
 public class BaseServiceImpl implements BaseService, BeanMapping {
 
     @Autowired
@@ -34,4 +41,34 @@ public class BaseServiceImpl implements BaseService, BeanMapping {
     public <T, MT> PageList<MT> mapping(@Nullable final PageList<T> pageList, @Nonnull final Class<MT> mtClass) {
         return mappingService.mapping(pageList, mtClass);
     }
+
+    protected <T, R> R validData(@Nullable final ResultVO<? extends T> ret, @Nonnull final Function<T, R> handler) {
+        if (Objects.nonNull(ret)) {
+            final boolean ok = ret.getCode() == ResultCode.Success.getVal();
+            if (!ok) {
+                throw new ServiceException(ret.getCode(), ret.getMessage());
+            }
+            return handler.apply(ret.getData());
+        }
+        return null;
+    }
+
+    protected <T> T validData(@Nullable final ResultVO<? extends T> ret) {
+        return validData(ret, t -> t);
+    }
+
+    protected <T, R> R ejectionData(@Nullable final ResultVO<? extends T> ret, @Nonnull final Function<T, R> handler) {
+        if (Objects.nonNull(ret)) {
+            if (ret.getCode() == ResultCode.Success.getVal()) {
+                return handler.apply(ret.getData());
+            }
+            log.warn("ejectionData(ret: {})", ret);
+        }
+        return null;
+    }
+
+    protected <T> T ejectionData(@Nullable final ResultVO<? extends T> ret) {
+        return ejectionData(ret, t -> t);
+    }
+
 }
