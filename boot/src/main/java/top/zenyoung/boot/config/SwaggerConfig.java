6a7adf1aa8d02028972fedf91f48c8393b81f484
yangyong <jeason1914@qq.com>
2023-03-19 13:55:15 +0800

    fix:[boot]修复Swagger问题

--- boot/src/main/java/top/zenyoung/boot/config/SwaggerConfig.java
+++ boot/src/main/java/top/zenyoung/boot/config/SwaggerConfig.java
@@ -1,47 +1,13 @@
 package top.zenyoung.boot.config;
 
-import com.github.xiaoymin.knife4j.core.extend.OpenApiExtendMarkdownFile;
-import com.github.xiaoymin.knife4j.spring.configuration.Knife4jProperties;
-import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtension;
-import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtensionResolver;
-import com.github.xiaoymin.knife4j.spring.extension.OpenApiMarkdownExtension;
-import com.github.xiaoymin.knife4j.spring.extension.OpenApiSettingExtension;
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import top.zenyoung.boot.registrar.SwaggerBeanCreator;
 import top.zenyoung.boot.util.EnvUtils;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * App 配置
@@ -51,7 +17,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 @Import({SwaggerProperties.class})
-public class SwaggerConfig implements InitializingBean, BeanPostProcessor {
+public class SwaggerConfig implements InitializingBean {
     private final ApplicationContext context;
 
     @Value("${spring.application.name:default}")
@@ -64,195 +30,41 @@ public class SwaggerConfig implements InitializingBean, BeanPostProcessor {
     private String cloudProfile;
 
     @Override
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet() {
         final boolean isDevOrTest = EnvUtils.isDevOrTest(bootProfile, cloudProfile);
         final SwaggerBeanCreator beanCreator = SwaggerBeanCreator.of(context, appName, isDevOrTest);
         beanCreator.create();
     }
 
-    @Override
-    public Object postProcessBeforeInitialization(@Nonnull final Object bean, @Nonnull final String beanName) throws BeansException {
-        return bean;
-    }
-
-    @Override
-    @SuppressWarnings({"unchecked"})
-    public Object postProcessAfterInitialization(@Nonnull final Object bean, @Nonnull final String beanName) throws BeansException {
-        //处理swagger在SpringBoot2.6以上的不可用问题
-        final DefaultListableBeanFactory defaultListableBeanFactory = context.getBean(DefaultListableBeanFactory.class);
-        final boolean modify = (bean instanceof WebMvcRequestHandlerProvider) && defaultListableBeanFactory.containsBean(beanName);
-        if (modify) {
-            try {
-                final Field handlerMappingsField = ReflectionUtils.findField(WebMvcRequestHandlerProvider.class, "handlerMappings");
-                if (Objects.nonNull(handlerMappingsField)) {
-                    handlerMappingsField.setAccessible(true);
-                    final List<RequestMappingInfoHandlerMapping> handlerMappings = (List<RequestMappingInfoHandlerMapping>) ReflectionUtils.getField(handlerMappingsField, bean);
-                    if (Objects.nonNull(handlerMappings)) {
-                        final List<RequestMappingInfoHandlerMapping> items = handlerMappings.stream().filter(m -> Objects.isNull(m.getPatternParser())).collect(Collectors.toList());
-                        handlerMappings.clear();
-                        if (!CollectionUtils.isEmpty(items)) {
-                            handlerMappings.addAll(items);
-                        }
-                    }
-                }
-            } catch (Throwable e) {
-                log.warn("修改WebMvcRequestHandlerProvider的属性:handlerMappings出错,可能导致Swagger不可用=> {}", e.getMessage());
-            }
-        }
-        return bean;
-    }
-
-    @RequiredArgsConstructor(staticName = "of")
-    private static class SwaggerBeanCreator {
-        private final ApplicationContext context;
-        private final String appName;
-        private final boolean isDevOrTest;
-
-        private SwaggerProperties properties;
-        private boolean knife4jEnable;
-
-        public void create() {
-            final DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) context.getAutowireCapableBeanFactory();
-            this.properties = context.getBean(SwaggerProperties.class);
-            this.knife4jEnable = this.properties.getKnife4j();
-            final String simpleName = Docket.class.getSimpleName();
-            final List<SwaggerProperties.SwaggerItemProperties> dockets = properties.getDockets();
-            if (!CollectionUtils.isEmpty(dockets)) {
-                for (int i = 0; i < dockets.size(); i++) {
-                    registerSyntheticBeanIfMissing(beanFactory, simpleName + "-" + i, dockets.get(i));
-                }
-                return;
-            }
-            registerSyntheticBeanIfMissing(beanFactory, simpleName, null);
-        }
-
-        private void registerSyntheticBeanIfMissing(@Nonnull final DefaultListableBeanFactory beanFactory,
-                                                    @Nonnull final String name,
-                                                    @Nullable final SwaggerProperties.SwaggerItemProperties item) {
-            final BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(
-                    Docket.class,
-                    () -> createDocket(item)
-            );
-            beanFactory.registerBeanDefinition(name, builder.getRawBeanDefinition());
-        }
-
-        private Docket createDocket(@Nullable final SwaggerProperties.SwaggerItemProperties item) {
-            if (Objects.isNull(item)) {
-                return defaultDocket();
-            }
-            String groupName = item.getGroup();
-            if(Strings.isNullOrEmpty(groupName)){
-                groupName = "default";
-            }
-            final Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                    .apiInfo(apiInfo())
-                    .groupName(groupName)
-                    .enable(isDevOrTest);
-            if(knife4jEnable){
-                final OpenApiExtensionResolver resolver = context.getBean(OpenApiExtensionResolver.class);
-                docket.extensions(resolver.buildExtensions(groupName));
-            }
-            final ApiSelectorBuilder builder = docket.select();
-            final String apis = item.getApis();
-            if(Strings.isNullOrEmpty(apis)){
-                builder.apis(RequestHandlerSelectors.any());
-            }else{
-                builder.apis(RequestHandlerSelectors.basePackage(apis));
-            }
-            final String paths = item.getPaths();
-            if(Strings.isNullOrEmpty(paths)){
-                builder.paths(PathSelectors.any());
-            }else{
-                builder.paths(PathSelectors.regex(paths));
-            }
-            builder.build();
-            return docket;
-        }
-
-        @SuppressWarnings({"unchecked"})
-        private static <T> T getFieldValue(@Nullable final OpenApiExtensionResolver resolver, @Nonnull final String name) {
-            if (Objects.nonNull(resolver) && !Strings.isNullOrEmpty(name)) {
-                final Field field = ReflectionUtils.findField(resolver.getClass(), name);
-                if (Objects.nonNull(field)) {
-                    try {
-                        return (T) field.get(resolver);
-                    } catch (Throwable e) {
-                        log.error("getFieldValue(resolver: {},name: {})[field: {}]-exp: {}", resolver, name, field, e.getMessage());
-                    }
-                }
-            }
-            return null;
-        }
-
-        private Docket defaultDocket() {
-            final Docket docket = new Docket(DocumentationType.SWAGGER_2);
-            if (knife4jEnable) {
-                final OpenApiExtensionResolver resolver = context.getBean(OpenApiExtensionResolver.class);
-                final Knife4jProperties knife4jProperties = context.getBean(Knife4jProperties.class);
-                final Map<String, List<OpenApiExtendMarkdownFile>> markdownFileMap = getFieldValue(resolver, "markdownFileMaps");
-                docket.extensions(buildExtensions(markdownFileMap, knife4jProperties));
-            }
-            final String apiPath = defaultApiPath();
-            docket.apiInfo(apiInfo())
-                    .groupName(appName)
-                    .enable(isDevOrTest)
-                    .select()
-                    .apis(RequestHandlerSelectors.basePackage(apiPath))
-                    .paths(PathSelectors.any())
-                    .build();
-            return docket;
-        }
-
-        private String defaultApiPath() {
-            String path = "top.zenyoung";
-            final Map<String, Object> annMap = context.getBeansWithAnnotation(ComponentScan.class);
-            if (CollectionUtils.isEmpty(annMap)) {
-                final List<String> suffixs = Lists.newArrayList("App", "Application");
-                for (Map.Entry<String, Object> entry : annMap.entrySet()) {
-                    final String key = entry.getKey();
-                    final Class<?> cls = entry.getValue().getClass();
-                    if (!Strings.isNullOrEmpty(key) && Objects.nonNull(cls)) {
-                        for (String suffix : suffixs) {
-                            if (key.endsWith(suffix)) {
-                                return cls.getPackage().getName();
-                            }
-                        }
-                    }
-                }
-            }
-            return path;
-        }
-
-        private ApiInfo apiInfo() {
-            final SwaggerProperties.ContactProperties p = properties.getContact();
-            Contact contact;
-            if (Objects.isNull(p)) {
-                contact = new Contact("zenyoung", "", "");
-            } else {
-                contact = new Contact(p.getName(), p.getUrl(), p.getEmail());
-            }
-            return new ApiInfoBuilder()
-                    .title(properties.getTitle())
-                    .termsOfServiceUrl(properties.getTermsOfServiceUrl())
-                    .contact(contact)
-                    .version(properties.getVersion())
-                    .build();
-        }
-
-        @SuppressWarnings({"all"})
-        private List<VendorExtension> buildExtensions(
-                @Nullable final Map<String, List<OpenApiExtendMarkdownFile>> markdownFileMaps,
-                @Nonnull final Knife4jProperties knife4jProperties) {
-            if (CollectionUtils.isEmpty(markdownFileMaps)) {
-                return Lists.newArrayList();
-            }
-            final List<OpenApiExtendMarkdownFile> all = markdownFileMaps.values().stream()
-                    .map(Lists::newArrayList)
-                    .collect(ArrayList::new, ArrayList::addAll, ArrayList::addAll);
-            final OpenApiExtension openApiExtension = new OpenApiExtension("x-openapi");
-            openApiExtension.addProperty(new OpenApiSettingExtension(knife4jProperties.getSetting()));
-            openApiExtension.addProperty(new OpenApiMarkdownExtension(all));
-            return Lists.newArrayList(openApiExtension);
-        }
-    }
+//    @Override
+//    public Object postProcessBeforeInitialization(@Nonnull final Object bean, @Nonnull final String beanName) throws BeansException {
+//        return bean;
+//    }
+//
+//    @Override
+//    @SuppressWarnings({"unchecked"})
+//    public Object postProcessAfterInitialization(@Nonnull final Object bean, @Nonnull final String beanName) throws BeansException {
+//        //处理swagger在SpringBoot2.6以上的不可用问题
+//        final DefaultListableBeanFactory defaultListableBeanFactory = context.getBean(DefaultListableBeanFactory.class);
+//        final boolean modify = (bean instanceof WebMvcRequestHandlerProvider) && defaultListableBeanFactory.containsBean(beanName);
+//        if (modify) {
+//            try {
+//                final Field handlerMappingsField = ReflectionUtils.findField(WebMvcRequestHandlerProvider.class, "handlerMappings");
+//                if (Objects.nonNull(handlerMappingsField)) {
+//                    handlerMappingsField.setAccessible(true);
+//                    final List<RequestMappingInfoHandlerMapping> handlerMappings = (List<RequestMappingInfoHandlerMapping>) ReflectionUtils.getField(handlerMappingsField, bean);
+//                    if (Objects.nonNull(handlerMappings)) {
+//                        final List<RequestMappingInfoHandlerMapping> items = handlerMappings.stream().filter(m -> Objects.isNull(m.getPatternParser())).collect(Collectors.toList());
+//                        handlerMappings.clear();
+//                        if (!CollectionUtils.isEmpty(items)) {
+//                            handlerMappings.addAll(items);
+//                        }
+//                    }
+//                }
+//            } catch (Throwable e) {
+//                log.warn("修改WebMvcRequestHandlerProvider的属性:handlerMappings出错,可能导致Swagger不可用=> {}", e.getMessage());
+//            }
+//        }
+//        return bean;
+//    }
 }
--- file-boot/src/main/java/top/zenyoung/file/service/impl/BucketFileServiceImpl.java
+++ file-boot/src/main/java/top/zenyoung/file/service/impl/BucketFileServiceImpl.java
@@ -1,7 +1,7 @@
 package top.zenyoung.file.service.impl;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import top.zenyoung.file.FileService;
@@ -13,7 +13,7 @@ import top.zenyoung.file.vo.FileVO;
 
 import javax.annotation.Nonnull;
 import java.io.InputStream;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 /**
  * 配置文件桶-文件服务接口实现
@@ -22,52 +22,55 @@ import java.util.function.Function;
  */
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class BucketFileServiceImpl implements BucketFileService {
-    private final FileService service;
-    private final FileProperties fileProperties;
+    @Autowired(required = false)
+    private FileService service;
+    @Autowired(required = false)
+    private FileProperties fileProperties;
 
-    private <R> R bucketHandler(@Nonnull final Function<String, R> handler) {
+    private <R> R handler(@Nonnull final BiFunction<FileService, String, R> handler) {
+        Assert.notNull(fileProperties, "'fileProperties未注入");
         final String bucket;
         Assert.hasText(bucket = fileProperties.getBucket(), "'bucket'未配置");
-        return handler.apply(bucket);
+        Assert.notNull(service, "'FileService'未注入");
+        return handler.apply(service, bucket);
     }
 
     @Override
     public boolean checkFileExists(@Nonnull final String key) {
-        return bucketHandler(bucket -> service.checkFileExists(bucket, key));
+        return handler((srv, bucket) -> srv.checkFileExists(bucket, key));
     }
 
     @Override
     public DirectVO directUploadSignature(@Nonnull final DirectDTO dto) {
-        return bucketHandler(bucket -> {
+        return handler((srv, bucket) -> {
             dto.setBucket(bucket);
-            return service.directUploadSignature(dto);
+            return srv.directUploadSignature(dto);
         });
     }
 
     @Override
     public FileVO upload(@Nonnull final String key, @Nonnull final String fileName, @Nonnull final InputStream input) {
-        return bucketHandler(bucket -> service.upload(bucket, key, fileName, input));
+        return handler((srv, bucket) -> srv.upload(bucket, key, fileName, input));
     }
 
     @Override
     public InputStream getFileStream(@Nonnull final String key) {
-        return bucketHandler(bucket -> service.getFileStream(bucket, key));
+        return handler((srv, bucket) -> srv.getFileStream(bucket, key));
     }
 
     @Override
     public void copyFile(@Nonnull final String sourceKey, @Nonnull final String targetKey) {
-        bucketHandler(bucket -> {
-            service.copyFile(bucket, sourceKey, bucket, targetKey);
+        handler((srv, bucket) -> {
+            srv.copyFile(bucket, sourceKey, bucket, targetKey);
             return null;
         });
     }
 
     @Override
     public void deleteFiles(@Nonnull final String... keys) {
-        bucketHandler(bucket -> {
-            service.deleteFiles(bucket, keys);
+        handler((srv, bucket) -> {
+            srv.deleteFiles(bucket, keys);
             return null;
         });
     }
