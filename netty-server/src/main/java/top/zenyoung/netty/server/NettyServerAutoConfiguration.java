339e234c5bcbfc02955a68c1d49de7bfa1ca2629
yangyong <jeason1914@qq.com>
2023-04-11 20:43:02 +0800

    fix:[netty]修复bug

--- netty-server/src/main/java/top/zenyoung/netty/server/NettyServerAutoConfiguration.java
+++ netty-server/src/main/java/top/zenyoung/netty/server/NettyServerAutoConfiguration.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import top.zenyoung.netty.codec.Message;
@@ -12,6 +13,7 @@ import top.zenyoung.netty.handler.StrategyFactoryInstance;
 import top.zenyoung.netty.server.config.AsyncEventConfig;
 import top.zenyoung.netty.server.config.NettyServerProperties;
 import top.zenyoung.netty.server.handler.StrategyHandler;
+import top.zenyoung.netty.server.server.NettyServer;
 
 import java.util.List;
 
@@ -23,11 +25,12 @@ import java.util.List;
 @Slf4j
 @Configuration
 @Import({AsyncEventConfig.class})
+@ComponentScan(basePackageClasses = {NettyServer.class})
 @EnableConfigurationProperties({NettyServerProperties.class})
 public class NettyServerAutoConfiguration {
 
-    @Bean("serverStrategyFactory")
     @ConditionalOnMissingBean
+    @Bean("serverStrategyFactory")
     public StrategyFactory strategyFactory(final List<StrategyHandler<? extends Message>> strategies) {
         return StrategyFactoryInstance.instance(strategies);
     }
