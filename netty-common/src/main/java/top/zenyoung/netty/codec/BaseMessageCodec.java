2ae7f14da6a6926854b54409030fe57461650abb
yangyong <jeason1914@qq.com>
2023-04-12 14:28:01 +0800

    fix:[netty]修复bug

--- netty-common/src/main/java/top/zenyoung/netty/codec/BaseMessageCodec.java
+++ netty-common/src/main/java/top/zenyoung/netty/codec/BaseMessageCodec.java
@@ -4,9 +4,6 @@ import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 
-import javax.annotation.Nonnull;
-import java.util.Objects;
-
 /**
  * 消息编解码器抽象基类
  *
@@ -14,49 +11,16 @@ import java.util.Objects;
  */
 public abstract class BaseMessageCodec<T extends BaseMessageEncoder<? extends Message>, R extends BaseMessageDecoder<? extends Message>>
         extends ChannelDuplexHandler implements MessageCodec<T, R> {
-    private final T encoder;
-    private final R decoder;
-
-    /**
-     * 构造函数
-     */
-    public BaseMessageCodec() {
-        preInit();
-        initCodec(decoder = getDecoder(), encoder = getEncoder());
-    }
 
     @Override
     public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
-        if (Objects.nonNull(decoder)) {
-            decoder.channelRead(ctx, msg);
-        } else {
-            super.channelRead(ctx, msg);
-        }
+        final R decoder = getDecoder();
+        decoder.channelRead(ctx, msg);
     }
 
     @Override
     public void write(final ChannelHandlerContext ctx, final Object msg, final ChannelPromise promise) throws Exception {
-        if (Objects.nonNull(encoder)) {
-            encoder.write(ctx, msg, promise);
-        } else {
-            super.write(ctx, msg, promise);
-        }
-    }
-
-    /**
-     * 初始化前置调用
-     */
-    protected void preInit() {
-
-    }
-
-    /**
-     * 初始化编解码器处理
-     *
-     * @param decoder 解码器
-     * @param encoder 编码器
-     */
-    protected void initCodec(@Nonnull final R decoder, @Nonnull final T encoder) {
-
+        final T encoder = getEncoder();
+        encoder.write(ctx, msg, promise);
     }
 }
