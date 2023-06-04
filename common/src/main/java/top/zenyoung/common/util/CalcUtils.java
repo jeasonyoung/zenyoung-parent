9b2a49ca8bedef5cdfcc8b31c606dc78e0380490
yangyong <jeason1914@qq.com>
2023-06-04 23:39:41 +0800

    fix: [common] 1.添加item => map 处理

--- common/src/main/java/top/zenyoung/common/util/CalcUtils.java
+++ common/src/main/java/top/zenyoung/common/util/CalcUtils.java
@@ -1,10 +1,12 @@
 package top.zenyoung.common.util;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -25,5 +27,25 @@ public class CalcUtils {
                     .collect(Collectors.toList());
         }
         return Lists.newArrayList();
+    }
+
+    public static <T, K, V> Map<K, V> map(@Nullable final List<T> items, @Nonnull final Function<T, K> keyConvert,
+                                          @Nonnull final Function<T, V> valConvert) {
+        if (items != null && items.size() > 0) {
+            return items.stream()
+                    .filter(t -> Objects.nonNull(keyConvert.apply(t)) && Objects.nonNull(valConvert.apply(t)))
+                    .collect(Collectors.toMap(keyConvert, valConvert, (o, n) -> n));
+        }
+        return Maps.newHashMap();
+    }
+
+    public static <T, K, V> Map<K, List<V>> group(@Nullable final List<T> items, @Nonnull final Function<T, K> keyConvert,
+                                                  @Nonnull final Function<T, V> valConvert) {
+        if (items != null && items.size() > 0) {
+            return items.stream()
+                    .filter(t -> Objects.nonNull(keyConvert.apply(t)) && Objects.nonNull(valConvert.apply(t)))
+                    .collect(Collectors.groupingBy(keyConvert, Collectors.mapping(valConvert, Collectors.toList())));
+        }
+        return Maps.newHashMap();
     }
 }
