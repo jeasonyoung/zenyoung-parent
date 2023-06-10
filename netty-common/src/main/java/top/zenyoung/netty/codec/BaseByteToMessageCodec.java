790ca4f75e01f8f39a45de2a11cd9086579ffc55
yangyong <jeason1914@qq.com>
2023-06-10 14:27:33 +0800

    fix: [netty-common] 1.优化编解码器基类

--- netty-common/src/main/java/top/zenyoung/netty/codec/BaseByteToMessageCodec.java
+++ netty-common/src/main/java/top/zenyoung/netty/codec/BaseByteToMessageCodec.java
@@ -1,6 +1,12 @@
 package top.zenyoung.netty.codec;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageCodec;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * 消息编解码器基类(byte<=>message)
@@ -10,4 +16,13 @@ import io.netty.handler.codec.ByteToMessageCodec;
  */
 public abstract class BaseByteToMessageCodec<T extends Message> extends ByteToMessageCodec<T> {
 
+    @Override
+    protected final void decode(final ChannelHandlerContext ctx, final ByteBuf in, final List<Object> out) {
+        final T data = decode(ctx, in);
+        if (Objects.nonNull(data)) {
+            out.add(data);
+        }
+    }
+
+    protected abstract T decode(@Nonnull final ChannelHandlerContext ctx, @Nonnull final ByteBuf in);
 }
