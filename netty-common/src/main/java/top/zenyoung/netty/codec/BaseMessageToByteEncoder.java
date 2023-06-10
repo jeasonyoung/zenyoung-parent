1f0e69dc9a4705aa5affd215cd798a900aeaa870
yangyong <jeason1914@qq.com>
2023-06-10 14:08:13 +0800

    fix: [netty-common] 1.优化编解码器基类

--- netty-common/src/main/java/top/zenyoung/netty/codec/BaseMessageToByteEncoder.java
+++ netty-common/src/main/java/top/zenyoung/netty/codec/BaseMessageToByteEncoder.java
@@ -1,5 +1,6 @@
 package top.zenyoung.netty.codec;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.handler.codec.MessageToByteEncoder;
 
 /**
@@ -7,6 +8,7 @@ import io.netty.handler.codec.MessageToByteEncoder;
  *
  * @author young
  */
+@ChannelHandler.Sharable
 public abstract class BaseMessageToByteEncoder<T extends Message> extends MessageToByteEncoder<T> {
 
 }
