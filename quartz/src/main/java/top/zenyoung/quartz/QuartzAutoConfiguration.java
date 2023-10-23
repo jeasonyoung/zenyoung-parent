56af0b43355cb98178e829089c2347ae3ac1c510
yangyong <jeason1914@qq.com>
2023-10-23 22:40:42 +0800

    [quartz] 1.优化实现

--- quartz/src/main/java/top/zenyoung/quartz/QuartzAutoConfiguration.java
+++ quartz/src/main/java/top/zenyoung/quartz/QuartzAutoConfiguration.java
@@ -3,10 +3,13 @@ package top.zenyoung.quartz;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Scheduler;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.zenyoung.quartz.job.TaskJobManager;
 import top.zenyoung.quartz.job.TaskJobManagerFactory;
+
+import javax.annotation.Nonnull;
 
 /**
  * 定时任务-自动配置
@@ -18,7 +21,8 @@ import top.zenyoung.quartz.job.TaskJobManagerFactory;
 public class QuartzAutoConfiguration {
 
     @Bean
-    public TaskJobManager taskJobManager(final ObjectProvider<Scheduler> provider) {
+    @ConditionalOnMissingBean
+    public TaskJobManager taskJobManager(@Nonnull final ObjectProvider<Scheduler> provider) {
         return TaskJobManagerFactory.of(provider.getIfAvailable());
     }
 }
