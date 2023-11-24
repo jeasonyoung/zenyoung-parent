3aac72b2c65e6fb754365233c3b3750a2f775f7f
yangyong <jeason1914@qq.com>
2023-11-24 16:43:01 +0800

    [jpa-reactive] 优化编译时生成代码

--- jpa-reactive/src/main/java/top/zenyoung/jpa/reactive/querydsl/apt/R2dbcAnnotationProcessor.java
+++ jpa-reactive/src/main/java/top/zenyoung/jpa/reactive/querydsl/apt/R2dbcAnnotationProcessor.java
@@ -1,213 +1,29 @@
 package top.zenyoung.jpa.reactive.querydsl.apt;
 
-import com.google.common.base.CaseFormat;
+import com.google.auto.service.AutoService;
 import com.querydsl.apt.AbstractQuerydslProcessor;
 import com.querydsl.apt.Configuration;
-import com.querydsl.apt.ExtendedTypeFactory;
-import com.querydsl.apt.TypeElementHandler;
-import com.querydsl.codegen.*;
-import com.querydsl.codegen.utils.model.Type;
-import com.querydsl.codegen.utils.model.TypeCategory;
-import com.querydsl.sql.codegen.NamingStrategy;
-import lombok.experimental.UtilityClass;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Embedded;
 import org.springframework.data.relational.core.mapping.Table;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.TypeMirror;
-import javax.lang.model.util.ElementFilter;
-import javax.lang.model.util.Elements;
-import javax.lang.model.util.Types;
 import java.lang.annotation.Annotation;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SupportedAnnotationTypes({
         "com.querydsl.core.annotations.*",
         "org.springframework.data.annotation.*",
         "org.springframework.data.relational.core.mapping.*"
 })
+@AutoService(Processor.class)
 public class R2dbcAnnotationProcessor extends AbstractQuerydslProcessor {
-    private static final CaseFormat columnCaseFormat = CaseFormat.UPPER_CAMEL;
-    private static final CaseFormat tableCaseFormat = CaseFormat.UPPER_CAMEL;
-    private RoundEnvironment roundEnv;
-    private Configuration conf;
-    private ExtendedTypeFactory typeFactory;
-
     @Override
     protected Configuration createConfiguration(final RoundEnvironment roundEnv) {
-        this.roundEnv = roundEnv;
-        //
         final Class<? extends Annotation> entity = Table.class;
         final Class<? extends Annotation> embedded = Embedded.class;
         final Class<? extends Annotation> skip = Transient.class;
-        //
-        final CodegenModule codegenModule = new CodegenModule();
-        final JavaTypeMappings typeMappings = new JavaTypeMappings();
-        codegenModule.bind(TypeMappings.class, typeMappings);
-        codegenModule.bind(QueryTypeFactory.class, new QueryTypeFactoryImpl("Q", "", ""));
-        //
-        final NamingStrategy namingStrategy = new R2dbcNamingStrategy();
-        //
-        this.conf = new R2dbcConfiguration(roundEnv, processingEnv, columnCaseFormat, entity,
-                null, null, embedded, skip, typeMappings, codegenModule, namingStrategy);
-        return this.conf;
-    }
-
-    @Override
-    protected ExtendedTypeFactory createTypeFactory(final Set<Class<? extends Annotation>> entityAnnotations,
-                                                    final TypeMappings typeMappings,
-                                                    final QueryTypeFactory queryTypeFactory) {
-        this.typeFactory = new CustomExtendedTypeFactory(processingEnv, entityAnnotations, typeMappings,
-                queryTypeFactory, this.conf, processingEnv.getElementUtils(), tableCaseFormat);
-        return this.typeFactory;
-    }
-
-    @Nonnull
-    @Override
-    protected TypeElementHandler createElementHandler(@Nonnull final TypeMappings typeMappings,
-                                                      @Nonnull final QueryTypeFactory queryTypeFactory) {
-
-
-        return new TypeElementHandler(this.conf, this.typeFactory, typeMappings, queryTypeFactory);
-    }
-
-    @Override
-    protected Set<TypeElement> collectElements() {
-        final Set<TypeElement> entityElements = roundEnv.getElementsAnnotatedWith(conf.getEntitiesAnnotation())
-                .stream()
-                .map(Element::getEnclosingElement)
-                .filter(TypeElement.class::isInstance)
-                .map(el -> (TypeElement) el)
-                .collect(Collectors.toSet());
-        return entityElements.stream()
-                .flatMap(this::getEntityElementWithEmbeddedEntities)
-                .collect(Collectors.toSet());
-    }
-
-    private Stream<TypeElement> getEntityElementWithEmbeddedEntities(@Nonnull final TypeElement entityElement) {
-        final Types types = processingEnv.getTypeUtils();
-        final Set<TypeElement> embeddedElements = ElementFilter.fieldsIn(entityElement.getEnclosedElements())
-                .stream()
-                .filter(el -> Objects.nonNull(el.getAnnotation(conf.getEmbeddedAnnotation())))
-                .map(el -> types.asElement(el.asType()))
-                .filter(TypeElement.class::isInstance)
-                .map(el -> (TypeElement) el)
-                .collect(Collectors.toSet());
-        return Stream.concat(Stream.of(entityElement), embeddedElements.stream());
-    }
-
-    @UtilityClass
-    private static class Embeddeds {
-        public static boolean isEmbedded(@Nonnull final Configuration configuration,
-                                         @Nonnull final Element element,
-                                         @Nonnull final Property property) {
-            final Class<? extends Annotation> embeddedAnnotation = configuration.getEmbeddedAnnotation();
-            if (Objects.isNull(embeddedAnnotation)) {
-                return false;
-            }
-            return ElementFilter.fieldsIn(element.getEnclosedElements())
-                    .stream()
-                    .filter(el -> el.getSimpleName().toString().equals(property.getName()))
-                    .allMatch(el -> Objects.nonNull(el.getAnnotation(embeddedAnnotation)));
-        }
-
-        public static boolean isEmbedded(@Nonnull final Configuration configuration, @Nonnull final Element element) {
-            final Class<? extends Annotation> embeddedAnnotation = configuration.getEmbeddedAnnotation();
-            if (Objects.isNull(embeddedAnnotation)) {
-                return false;
-            }
-            return Objects.nonNull(element.getAnnotation(embeddedAnnotation));
-        }
-    }
-
-    private static class CustomExtendedTypeFactory extends ExtendedTypeFactory {
-        public static final String IS_EMBEDDED_DATA_KEY = "isEmbedded";
-        private final Configuration configuration;
-        private final Elements elements;
-        private final CaseFormat tableCaseFormat;
-        private final Types types;
-
-        public CustomExtendedTypeFactory(@Nonnull final ProcessingEnvironment env,
-                                         @Nonnull final Set<Class<? extends Annotation>> annotations,
-                                         @Nonnull final TypeMappings typeMappings,
-                                         @Nonnull final QueryTypeFactory queryTypeFactory,
-                                         @Nonnull final Configuration configuration,
-                                         @Nonnull final Elements elements,
-                                         @Nonnull final CaseFormat tableCaseFormat) {
-            super(env, annotations, typeMappings, queryTypeFactory, configuration.getVariableNameFunction());
-            this.types = env.getTypeUtils();
-            this.configuration = configuration;
-            this.elements = elements;
-            this.tableCaseFormat = tableCaseFormat;
-        }
-
-        @Override
-        public boolean isSimpleTypeEntity(@Nonnull final TypeElement typeElement, @Nonnull final Class<? extends Annotation> entityAnn) {
-            return typeElement.getAnnotation(entityAnn) != null ||
-                    typeElement.getEnclosedElements().stream().anyMatch(el -> el.getAnnotation(entityAnn) != null);
-        }
-
-        @Nullable
-        @Override
-        public EntityType getEntityType(final TypeMirror typeMirror, final boolean deep) {
-            final Element element = types.asElement(typeMirror);
-            final EntityType entityType = super.getEntityType(typeMirror, deep);
-            if (Objects.nonNull(element) && Objects.nonNull(entityType)) {
-                final List<Property> embeddedlessProperties = entityType.getProperties().stream()
-                        .flatMap(property -> {
-                            if (Embeddeds.isEmbedded(configuration, element, property)) {
-                                final Type type = property.getType();
-                                if (!type.getCategory().equals(TypeCategory.ENTITY)) {
-                                    return Stream.of(property);
-                                }
-                                return ((EntityType) type).getProperties().stream();
-                            }
-                            return Stream.of(property);
-                        })
-                        .collect(Collectors.toList());
-                entityType.getProperties().clear();
-                entityType.getProperties().addAll(embeddedlessProperties);
-                entityType.getPropertyNames().clear();
-                entityType.getPropertyNames().addAll(embeddedlessProperties.stream()
-                        .map(Property::getName)
-                        .collect(Collectors.toList())
-                );
-                updateModel(element, entityType);
-            }
-            return entityType;
-        }
-
-        private void updateModel(@Nonnull final Element element, @Nonnull final EntityType type) {
-            final Map<Object, Object> data = type.getData();
-            data.put("table", getTableName(type));
-            type.getProperties().forEach(property -> {
-                if (Embeddeds.isEmbedded(configuration, element, property)) {
-                    property.getData().put(IS_EMBEDDED_DATA_KEY, true);
-                }
-            });
-        }
-
-        protected String getTableName(@Nonnull final EntityType model) {
-            final String simpleName = model.getSimpleName(),
-                    className = model.getPackageName() + "." + simpleName;
-            final String tableName = CaseFormat.UPPER_CAMEL.to(tableCaseFormat, simpleName);
-            return Optional.ofNullable(elements.getTypeElement(className).getAnnotation(Table.class))
-                    .map(table -> {
-                        if (table.value().isEmpty()) {
-                            return table.name();
-                        }
-                        return table.value();
-                    })
-                    .orElse(tableName);
-        }
+        return new R2dbcConfiguration(roundEnv, processingEnv, entity, null, null, embedded, skip);
     }
 }
--- jpa-reactive/src/main/java/top/zenyoung/jpa/reactive/querydsl/apt/R2dbcConfiguration.java
+++ jpa-reactive/src/main/java/top/zenyoung/jpa/reactive/querydsl/apt/R2dbcConfiguration.java
@@ -1,172 +1,114 @@
 package top.zenyoung.jpa.reactive.querydsl.apt;
 
-import com.google.common.base.CaseFormat;
 import com.google.common.collect.Lists;
-import com.querydsl.apt.APTOptions;
 import com.querydsl.apt.DefaultConfiguration;
+import com.querydsl.apt.TypeUtils;
 import com.querydsl.apt.VisitorConfig;
-import com.querydsl.codegen.*;
-import com.querydsl.codegen.utils.CodeWriter;
+import com.querydsl.codegen.Keywords;
 import com.querydsl.core.annotations.QueryEntities;
-import com.querydsl.sql.ColumnMetadata;
-import com.querydsl.sql.codegen.MetaDataSerializer;
-import com.querydsl.sql.codegen.NamingStrategy;
-import com.querydsl.sql.codegen.SQLCodegenModule;
-import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.AccessType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Embedded;
 import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
-import javax.lang.model.util.ElementFilter;
-import java.io.IOException;
+import javax.lang.model.element.*;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Types;
 import java.lang.annotation.Annotation;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 public class R2dbcConfiguration extends DefaultConfiguration {
-    private final SQLCodegenModule sqlCodegenModule;
+    private final List<Class<? extends Annotation>> annotations;
+    private final Types types;
 
     public R2dbcConfiguration(@Nonnull final RoundEnvironment roundEnv,
                               @Nonnull final ProcessingEnvironment processingEnv,
-                              @Nonnull final CaseFormat columnCaseFormat,
                               @Nonnull final Class<? extends Annotation> entityAnn,
                               @Nullable final Class<? extends Annotation> superTypeAnn,
                               @Nullable final Class<? extends Annotation> embeddableAnn,
                               @Nullable final Class<? extends Annotation> embeddedAnn,
-                              @Nullable final Class<? extends Annotation> skipAnn,
-                              @Nonnull final TypeMappings typeMappings,
-                              @Nonnull final CodegenModule codegenModule,
-                              @Nonnull final NamingStrategy namingStrategy) {
-        super(processingEnv, roundEnv, processingEnv.getOptions(), Keywords.JPA, QueryEntities.class, entityAnn,
-                superTypeAnn, embeddableAnn, embeddedAnn, skipAnn, codegenModule);
-        super.setStrictMode(true);
-        this.sqlCodegenModule = new SQLCodegenModule();
-        final Class<? extends Annotation> generatedAnnotationClass = GeneratedAnnotationResolver.resolve(
-                processingEnv.getOptions().get(APTOptions.QUERYDSL_GENERATED_ANNOTATION_CLASS)
-        );
-        this.sqlCodegenModule.bindInstance(CodegenModule.GENERATED_ANNOTATION_CLASS, generatedAnnotationClass);
-        this.sqlCodegenModule.bind(NamingStrategy.class, namingStrategy);
-        this.sqlCodegenModule.bind(TypeMappings.class, typeMappings);
-        this.sqlCodegenModule.bind(ProcessingEnvironment.class, processingEnv);
-        this.sqlCodegenModule.bind(CaseFormat.class, columnCaseFormat);
-        this.sqlCodegenModule.bind(Serializer.class, CustomMetaDataSerializer.class);
+                              @Nullable final Class<? extends Annotation> skipAnn) {
+        super(processingEnv, roundEnv, Keywords.JPA, QueryEntities.class, entityAnn,
+                superTypeAnn, embeddableAnn, embeddedAnn, skipAnn);
+        this.annotations = getAnnotations();
+        this.types = processingEnv.getTypeUtils();
+        setStrictMode(true);
     }
 
-    @Override
-    public boolean isBlockedField(@Nonnull final VariableElement field) {
-        if (field.getAnnotation(MappedCollection.class) != null) {
-            return true;
-        }
-        return super.isBlockedField(field);
+    protected List<Class<? extends Annotation>> getAnnotations() {
+        return Collections.unmodifiableList(Lists.newArrayList(
+                AccessType.class, Id.class, Transient.class, Version.class,
+                Column.class, Embedded.class, MappedCollection.class, Table.class
+        ));
     }
 
     @Override
     public VisitorConfig getConfig(@Nonnull final TypeElement e, @Nonnull final List<? extends Element> elements) {
-        return VisitorConfig.FIELDS_ONLY;
-    }
-
-    @Override
-    public Serializer getEntitySerializer() {
-        return this.sqlCodegenModule.get(Serializer.class);
-    }
-
-    @Override
-    public SerializerConfig getSerializerConfig(@Nonnull final EntityType entityType) {
-        return SimpleSerializerConfig.DEFAULT;
-    }
-
-    public static class CustomMetaDataSerializer extends MetaDataSerializer {
-        private final ProcessingEnvironment processingEnvironment;
-        private final CaseFormat columnCaseFormat;
-
-        @Inject
-        public CustomMetaDataSerializer(@Nonnull final TypeMappings typeMappings,
-                                        @Nonnull final NamingStrategy namingStrategy,
-                                        @Nonnull final ProcessingEnvironment processingEnvironment,
-                                        @Nonnull final CaseFormat columnCaseFormat,
-                                        @Named(SQLCodegenModule.INNER_CLASSES_FOR_KEYS) boolean innerClassesForKeys,
-                                        @Named(SQLCodegenModule.IMPORTS) Set<String> imports,
-                                        @Named(SQLCodegenModule.COLUMN_COMPARATOR) Comparator<Property> columnComparator,
-                                        @Named(SQLCodegenModule.ENTITYPATH_TYPE) Class<?> entityPathType,
-                                        @Named(SQLCodegenModule.GENERATED_ANNOTATION_CLASS) Class<? extends Annotation> generatedAnnotationClass) {
-            super(typeMappings, namingStrategy, innerClassesForKeys, imports, columnComparator, entityPathType, generatedAnnotationClass);
-            this.processingEnvironment = processingEnvironment;
-            this.columnCaseFormat = columnCaseFormat;
-        }
-
-        @Override
-        protected void serializeProperties(@Nonnull final EntityType model, @Nonnull final SerializerConfig config,
-                                           @Nonnull final CodeWriter writer) throws IOException {
-            final CustomPropertiesEntityType newModel = withFieldOrderedProperties(model);
-            super.serializeProperties(newModel, config, writer);
-        }
-
-        @Override
-        protected void outro(@Nonnull final EntityType model, @Nonnull final CodeWriter writer) throws IOException {
-            final CustomPropertiesEntityType newModel = withFieldOrderedProperties(model);
-            super.outro(newModel, writer);
-        }
-
-        private CustomPropertiesEntityType withFieldOrderedProperties(@Nonnull final EntityType entityType) {
-            final Set<Property> properties = entityType.getProperties();
-            final Map<String, Property> propertyNameToProperty = properties.stream()
-                    .collect(Collectors.toMap(Property::getName, Function.identity(), (o, n) -> n));
-            final TypeElement typeElement = this.processingEnvironment.getElementUtils().getTypeElement(entityType.getFullName());
-            final Set<Property> orderedProperties = ElementFilter.fieldsIn(Lists.newArrayList(typeElement.getEnclosedElements()))
-                    .stream()
-                    .map(field -> propertyNameToProperty.get(field.getSimpleName().toString()))
-                    .filter(Objects::nonNull)
-                    .collect(Collectors.toCollection(LinkedHashSet::new));
-            properties.stream()
-                    .filter(property -> !orderedProperties.contains(property))
-                    .forEach(orderedProperties::add);
-            //
-            final List<Property> orderedPropertiesList = Lists.newArrayList(orderedProperties);
-            for (int i = 0; i < orderedPropertiesList.size(); i++) {
-                final Property property = orderedPropertiesList.get(i);
-                property.getData().put("COLUMN", ColumnMetadata.named(getColumnName(property)).withIndex(i));
+        final AccessType access = e.getAnnotation(AccessType.class);
+        if (Objects.nonNull(access)) {
+            if (access.value() == AccessType.Type.FIELD) {
+                return VisitorConfig.FIELDS_ONLY;
+            } else {
+                return VisitorConfig.METHODS_ONLY;
             }
-            //
-            return new CustomPropertiesEntityType(entityType, orderedProperties);
         }
-
-        protected String getColumnName(@Nonnull final Property property) {
-            final TypeElement parentType = processingEnvironment.getElementUtils()
-                    .getTypeElement(property.getDeclaringType().getFullName());
-            return parentType.getEnclosedElements()
-                    .stream()
-                    .filter(VariableElement.class::isInstance)
-                    .map(el -> (VariableElement) el)
-                    .filter(el -> el.getSimpleName().toString().equals(property.getName()))
-                    .filter(el -> el.getAnnotation(Column.class) != null)
-                    .map(el -> el.getAnnotation(Column.class).value())
-                    .findAny()
-                    .orElseGet(() -> CaseFormat.LOWER_CAMEL.to(columnCaseFormat, property.getName()));
-
+        boolean fields = false, methods = false;
+        for (Element element : elements) {
+            if (hasRelevantAnnotation(element)) {
+                fields |= element.getKind() == ElementKind.FIELD;
+                methods |= element.getKind() == ElementKind.METHOD;
+            }
         }
+        return VisitorConfig.get(fields, methods, VisitorConfig.ALL);
     }
 
-    @EqualsAndHashCode(callSuper = true)
-    private static class CustomPropertiesEntityType extends EntityType {
-        private final Set<Property> fieldOrderedProperties;
+    @Override
+    public TypeMirror getRealType(@Nonnull final ExecutableElement method) {
+        return getRealElementType(method);
+    }
 
-        CustomPropertiesEntityType(@Nonnull final EntityType entityType, @Nonnull final Set<Property> fieldOrderedProperties) {
-            super(entityType);
-            this.fieldOrderedProperties = fieldOrderedProperties;
-        }
+    @Override
+    public TypeMirror getRealType(@Nonnull final VariableElement field) {
+        return getRealElementType(field);
+    }
 
-        @Override
-        public Set<Property> getProperties() {
-            return this.fieldOrderedProperties;
+    @Nullable
+    private TypeMirror getRealElementType(@Nonnull final Element element) {
+        AnnotationMirror mirror = TypeUtils.getAnnotationMirrorOfType(element, MappedCollection.class);
+        if (Objects.nonNull(mirror)) {
+            final TypeMirror typeArg = TypeUtils.getAnnotationValueAsTypeMirror(mirror, "targetEntity");
+            final TypeMirror erasure = (element instanceof ExecutableElement) ? ((ExecutableElement) element).getReturnType() :
+                    types.erasure(element.asType());
+            final TypeElement typeElement = (TypeElement) types.asElement(erasure);
+            if (typeElement != null && typeArg != null) {
+                if (typeElement.getTypeParameters().size() == 1) {
+                    return types.getDeclaredType(typeElement, typeArg);
+                } else if (typeElement.getTypeParameters().size() == 2 && (element.asType() instanceof DeclaredType)) {
+                    final TypeMirror first = ((DeclaredType) element.asType()).getTypeArguments().get(0);
+                    return types.getDeclaredType(typeElement, first, typeArg);
+                }
+            }
         }
+        return null;
+    }
+
+    private boolean hasRelevantAnnotation(@Nonnull final Element element) {
+        for (Class<? extends Annotation> annotation : annotations) {
+            if (element.getAnnotation(annotation) != null) {
+                return true;
+            }
+        }
+        return false;
     }
 }
