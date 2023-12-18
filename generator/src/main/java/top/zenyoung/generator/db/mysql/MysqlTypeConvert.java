6e471154e9c62cb15239d1b20fb7cdf26c4a064f
yangyong <jeason1914@qq.com>
2023-12-18 10:50:51 +0800

    fix[common]:优化处理

--- generator/src/main/java/top/zenyoung/generator/db/mysql/MysqlTypeConvert.java
+++ generator/src/main/java/top/zenyoung/generator/db/mysql/MysqlTypeConvert.java
@@ -1,6 +1,5 @@
 package top.zenyoung.generator.db.mysql;
 
-import top.zenyoung.common.convert.TypeConvertRunner;
 import top.zenyoung.generator.type.ColumnType;
 import top.zenyoung.generator.type.DbTypeConvert;
 
@@ -17,20 +16,18 @@ public class MysqlTypeConvert implements DbTypeConvert {
 
     @Override
     public ColumnType processTypeConvert(@Nonnull final String fieldType) {
-        final TypeConvertRunner<ColumnType> runner = new TypeConvertRunner<>();
-        return runner.use(fieldType)
-                .test(runner.containsAny("char", "text", "json", "enum").then(STRING))
-                .test(runner.contains("bigint").then(LONG))
-                .test(runner.containsAny("tinyint(1)", "bit(1)").then(INTEGER))
-                .test(runner.contains("bit").then(BYTE))
-                .test(runner.contains("int").then(INTEGER))
-                .test(runner.contains("decimal").then(BIG_DECIMAL))
-                .test(runner.contains("clob").then(CLOB))
-                .test(runner.contains("blob").then(BLOB))
-                .test(runner.contains("binary").then(BYTE_ARRAY))
-                .test(runner.contains("float").then(FLOAT))
-                .test(runner.contains("double").then(DOUBLE))
-                .test(runner.containsAny("date", "time", "year").then(DATE))
+        return ConvertRunner.of(fieldType)
+                .test("char", "text", "json", "enum").then(STRING)
+                .test("tinyint(1)", "bit(1)", "int").then(INTEGER)
+                .test("bigint").then(LONG)
+                .test("bit").then(BYTE)
+                .test("decimal").then(BIG_DECIMAL)
+                .test("clob").then(CLOB)
+                .test("blob").then(BLOB)
+                .test("binary").then(BYTE_ARRAY)
+                .test("float").then(FLOAT)
+                .test("double").then(DOUBLE)
+                .test("date", "time", "year").then(DATE)
                 .or(STRING);
     }
 }
--- generator/src/main/java/top/zenyoung/generator/type/DbTypeConvert.java
+++ generator/src/main/java/top/zenyoung/generator/type/DbTypeConvert.java
@@ -1,12 +1,56 @@
 package top.zenyoung.generator.type;
 
-import top.zenyoung.common.convert.TypeConvert;
+import com.google.common.base.Strings;
+import com.google.common.collect.Sets;
+import lombok.RequiredArgsConstructor;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * 数据库类型转换
  *
  * @author young
  */
-public interface DbTypeConvert extends TypeConvert<ColumnType> {
+public interface DbTypeConvert {
+    /**
+     * 执行类型转换
+     *
+     * @param fieldType 字段类型
+     * @return 转换结果类型
+     */
+    ColumnType processTypeConvert(@Nonnull final String fieldType);
 
+    /**
+     * 转换处理
+     */
+    @RequiredArgsConstructor(staticName = "of")
+    class ConvertRunner {
+        private final String fieldType;
+        private boolean selected = false;
+        private ColumnType ret = null;
+
+        public ConvertRunner test(@Nonnull final String... contains) {
+            if (this.selected) {
+                return this;
+            }
+            if (!Strings.isNullOrEmpty(this.fieldType) && contains.length > 0) {
+                final Set<String> items = Sets.newHashSet(contains);
+                this.selected = items.contains(this.fieldType);
+            }
+            return this;
+        }
+
+        public ConvertRunner then(@Nonnull final ColumnType ret) {
+            if (this.selected) {
+                this.ret = ret;
+            }
+            return this;
+        }
+
+        public ColumnType or(final ColumnType colType) {
+            return Optional.ofNullable(this.ret).orElse(colType);
+        }
+    }
 }
