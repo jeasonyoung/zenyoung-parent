3fe79c82b3b430b3ed27b3b5f23f439850a6511e
yangyong <jeason1914@qq.com>
2025-04-17 17:33:06 +0800

    fix:[boot-common]修复枚举参数处理

--- boot-common/src/main/java/top/zenyoung/boot/converter/EnumValueConvertFactory.java
+++ boot-common/src/main/java/top/zenyoung/boot/converter/EnumValueConvertFactory.java
@@ -1,11 +1,13 @@
 package top.zenyoung.boot.converter;
 
+import com.google.common.base.Strings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 import top.zenyoung.common.model.EnumValue;
 
 import javax.annotation.Nonnull;
+import java.util.regex.Pattern;
 
 /**
  * 枚举值转换工厂
@@ -13,15 +15,20 @@ import javax.annotation.Nonnull;
  * @author young
  */
 @RequiredArgsConstructor(staticName = "of")
-public class EnumValueConvertFactory implements ConverterFactory<Integer, EnumValue> {
+public class EnumValueConvertFactory implements ConverterFactory<String, EnumValue> {
     @Override
     @SuppressWarnings({"all"})
-    public <T extends EnumValue> Converter<Integer, T> getConverter(@Nonnull final Class<T> targetType) {
+    public <T extends EnumValue> Converter<String, T> getConverter(@Nonnull final Class<T> targetType) {
         return source -> {
-            final T[] enums = targetType.getEnumConstants();
-            for (final T e : enums) {
-                if (e.getVal() == source) {
-                    return e;
+            if (!Strings.isNullOrEmpty(source)) {
+                final T[] enums = targetType.getEnumConstants();
+                if (Pattern.matches("\\d+", source)) {
+                    final int val = Integer.parseInt(source);
+                    for (final T e : enums) {
+                        if (e.getVal() == val) {
+                            return e;
+                        }
+                    }
                 }
             }
             return null;
